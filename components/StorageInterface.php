f1822271219f56a5f68cd4d4fdaa3e31f5857187
e.nohroozian <inspiration988@yahoo.com>
2022-04-12 12:08:49 +0430

    +methods to interface

--- components/StorageInterface.php
+++ components/StorageInterface.php
@@ -1,7 +1,36 @@
 <?php
 
 
+namespace app\components;
+
+
+use yii\db\ActiveRecord;
+
 interface StorageInterface
 {
+
+    /**
+     * @param ActiveRecord $model
+     * @param array $data
+     * @param int $step
+     */
+    public static function save(ActiveRecord $model, array $data, int $step): void;
+
+    /**
+     * @param string $attributeName
+     * @param mixed $defualtValue
+     * @return mixed
+     */
+    public static function getValue(string $attributeName ,mixed $defualtValue = null);
+
+
+    /**
+     *
+     */
+    public static function destroy(): void ;
+
+
+
+
 
 }
